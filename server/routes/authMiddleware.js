d9a3fbcb545707eabee39fa940a7caf005971697
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-20 14:30:25 +0900

    認証用ミドルウェア作成

---
+++ server/routes/authMiddleware.js
@@ -0,0 +1,17 @@
+const Session = require('../models/Sessions');
+
+const authenticate = async (req, res, next) => {
+  const token = req.cookies.session_token;
+  if (!token) {
+    return res.status(401).json({ message: 'ログインしていません' });
+  }
+  const session = await Session.findToken(token);
+
+  if (!session || session.expires_at < new Date()) {
+    return res.status(401).json({ message: '無効なセッション' });
+  }
+  req.user = { email: session.email };
+  next();
+};
+
+module.exports = { authenticate };
---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate } = require('./authMiddleware');
+const { myAccount } = require('../controllers/usersController');
+
+router.use(authenticate);
+router.get('/myAccount', myAccount);
+
+module.exports = router;
+
+myAccount;
