386653f8f6259c04cbbfb3b7b63b43cabca02771
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-20 16:26:27 +0900

    追加忘れ　children testfile

---
+++ test/children.test.js
@@ -0,0 +1,79 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const db = require('../server/db/index');
+
+const app = require('../server.js');
+const expect = chai.expect;
+
+describe('childrenController', () => {
+  let request;
+  const userDummyData = {
+    email: 'fuga@fuga.com',
+    name: 'mocha',
+    password: 'password12345',
+  };
+  before(() => {
+    request = chai.request(app).keepOpen();
+  });
+
+  before(async () => {
+    await db.migrate
+      .forceFreeMigrationsLock()
+      .then(() => db.migrate.rollback({ all: true }))
+      .then(() => db.migrate.latest())
+      .then(() => db.seed.run())
+      .catch(console.error);
+    await request.post('/api/auth/registrater').send(userDummyData);
+  });
+
+  after(() => {
+    request.close();
+  });
+
+  describe('add', () => {
+    it('子供が追加できる', async () => {
+      const childData = {
+        user_id: 2,
+        name: 'testBaby',
+        birthday: '2024-01-01',
+        gender: 'f',
+      };
+      const expected = {
+        message: '子供の追加に成功しました',
+        newChildName: 'testBaby',
+      };
+      const resLogin = await request.post('/api/auth/login').send({
+        email: userDummyData.email,
+        password: userDummyData.password,
+      });
+      const res = await request
+        .post('/api/children/add')
+        .set('Cookie', `session_token=${resLogin.body.token}`)
+        .send(childData);
+      expect(res.statusCode).to.equal(201);
+      expect(res.body).to.deep.equal(expected);
+    });
+
+    it('すでに登録されている場合はエラー(現状は)', async () => {
+      const childData = {
+        user_id: 1,
+        name: 'testBaby',
+        birthday: '2024-01-01',
+        gender: 'f',
+      };
+      const expected = { message: '今は一人しか追加できません！今はね。。。' };
+      const resLogin = await request.post('/api/auth/login').send({
+        email: userDummyData.email,
+        password: userDummyData.password,
+      });
+      const res = await request
+        .post('/api/children/add')
+        .set('Cookie', `session_token=${resLogin.body.token}`)
+        .send(childData);
+      expect(res.statusCode).to.equal(400);
+      expect(res.body).to.deep.equal(expected);
+    });
+  });
+});
