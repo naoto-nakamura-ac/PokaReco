4fabb79ef87bf4e209c5fdb4973ae8a0e238a57d
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-21 19:37:16 +0900

    YamadaUI 導入

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // import './index.css'
+import { UIProvider, Box } from '@yamada-ui/react';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UIProvider>
+      <Box bg="#FFF4D8" minH="100vh">
+        <App />
+      </Box>
+    </UIProvider>
   </StrictMode>
 );
