6a8cfcdeb6e7030f30fa0a0cb0df43c20606c228
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-21 14:36:21 +0900

    デバッグ用ボタン設置

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -1,5 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 function Dashboard() {
-  return <div>dashboard</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>dashboard</div>
+
+      <button
+        onClick={async () => {
+          const res = await fetch('/api/auth/logout', { method: 'POST' });
+          const data = await res.json();
+          console.log(data.message);
+          navigate('/');
+        }}
+      >
+        デバッグ用ログアウトボタン
+      </button>
+    </>
+  );
 }
 
 export default Dashboard;
