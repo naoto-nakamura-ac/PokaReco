dd15a5322ef1901d334cbd1ff33fb5c0d85d1adf
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-21 14:27:54 +0900

    認証状態確認用API作成

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { login, logout, register } = require('../controllers/authController');
+const {
+  login,
+  logout,
+  register,
+  authMe,
+} = require('../controllers/authController');
+const { authenticate } = require('./authMiddleware');
 
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/register', register);
+router.get('/me', authenticate, authMe);
 
 module.exports = router;
