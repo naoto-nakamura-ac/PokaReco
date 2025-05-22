db7474802147149311a9a318b0b40aef963bbd72
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-22 19:44:47 +0900

    修正

--- front/src/components/MyAccount.jsx
+++ front/src/components/MyAccount.jsx
@@ -10,10 +10,11 @@ import {
   Box,
   Separator,
   Image,
+  Center,
 } from '@yamada-ui/react';
 import { useState, useEffect } from 'react';
 
-function MyAccount({ user }) {
+function MyAccount({ user, maxW = 'full' }) {
   const { name, email, children } = user;
   const [url, setUrl] = useState(null);
   const childrenNum = children?.length;
@@ -29,53 +30,55 @@ function MyAccount({ user }) {
   }, []);
 
   return (
-    <Card bg="white" padding="md">
-      <CardHeader justifyContent="center">あなたの情報</CardHeader>
-      <CardBody>
-        <HStack alignItems="center" gap="lg">
-          <VStack>
-            <Avatar
-              name={name}
-              size="xl"
-              // #TODO 仮で決めうち。アップロード機能作成する
-              // src="https://avatars.githubusercontent.com/u/84060430?v=4"
-            >
-              <AvatarBadge
-                bg="green"
-                ping
-                pingColor="green.300"
-                pingScale={2}
-              />
-            </Avatar>
-            <Box textAlign="center" lineHeight="1.2">
-              <Text fontSize="5xl" fontWeight="black" fontFamily="mono">
-                {name}
-              </Text>
-              <Text fontSize="sm" color="gray.600">
-                {email}
-              </Text>
-            </Box>
-          </VStack>
-          <VStack>
-            <Box textAlign="center" lineHeight="1.2">
-              <Text fontSize="sm" color="gray.600">
-                子ども
-              </Text>
-              <Text fontSize="2xl" fontWeight="bold" mt="1">
-                {childrenNum}人
-              </Text>
-            </Box>
-            <Separator variant="solid" />
-            <Box textAlign="center" lineHeight="1.2">
-              <Text fontSize="sm" color="gray.600">
-                名古屋の天気
-              </Text>
-              <Image src={url} />
-            </Box>
-          </VStack>
-        </HStack>
-      </CardBody>
-    </Card>
+    <Box maxW={maxW} w="full">
+      <Card bg="whiteAlpha.600" padding="sm">
+        <CardHeader justifyContent="center">あなたの情報</CardHeader>
+        <CardBody display="flex" justifyContent="center" alignItems="center">
+          <HStack alignItems="center" gap="lg">
+            <VStack>
+              <Avatar
+                name={name}
+                size="xl"
+                // #TODO 仮で決めうち。アップロード機能作成する
+                // src="https://avatars.githubusercontent.com/u/84060430?v=4"
+              >
+                <AvatarBadge
+                  bg="green"
+                  ping
+                  pingColor="green.300"
+                  pingScale={2}
+                />
+              </Avatar>
+              <Box textAlign="center" lineHeight="1.2">
+                <Text fontSize="5xl" fontWeight="black" fontFamily="mono">
+                  {name}
+                </Text>
+                <Text fontSize="sm" color="gray.600">
+                  {email}
+                </Text>
+              </Box>
+            </VStack>
+            <VStack>
+              <Box textAlign="center" lineHeight="1.2">
+                <Text fontSize="sm" color="gray.600">
+                  子ども
+                </Text>
+                <Text fontSize="2xl" fontWeight="bold" mt="1">
+                  {childrenNum}人
+                </Text>
+              </Box>
+              <Separator variant="solid" />
+              <Box textAlign="center" lineHeight="1.2">
+                <Text fontSize="sm" color="gray.600">
+                  名古屋の天気
+                </Text>
+                <Image src={url} />
+              </Box>
+            </VStack>
+          </HStack>
+        </CardBody>
+      </Card>
+    </Box>
   );
 }
 
